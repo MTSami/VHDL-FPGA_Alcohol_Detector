6d9701e1a3f57287b36827664535e27a67e7e952
MTSami <43340055+MTSami@users.noreply.github.com>
2018-09-17 18:36:44 +0600

    Add files via upload

---
+++ Test.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Mirza Tanzim Sami
+-- 
+-- Create Date:    01:15:46 08/03/2018 
+-- Design Name: 
+-- Module Name:    Test - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Test is
+    Port ( Switch : in  STD_LOGIC_VECTOR (5 downto 0);
+           LED : out  STD_LOGIC_VECTOR (7 downto 0);
+           SevenSegment : out  STD_LOGIC_VECTOR (7 downto 0));
+end Test;
+
+architecture Behavioral of Test is
+
+begin
+
+
+end Behavioral;
+
